be329b023c88f0b1d54ec9f807725c632698deca
NukoKRenard <skeanobo@gmail.com>
2025-03-03 23:07:16 -0500

    fix num whatever

--- assets/scripts/homepage-rive.js
+++ assets/scripts/homepage-rive.js
@@ -1,5 +1,5 @@
 const r = new rive.Rive({
-    src: "assets/rive/process-anim.riv",
+    src: "https://nukokrenard.github.io/Sun-Bird-Designs/assets/rive/process-anim.riv",
     // OR the path to a discoverable and public Rive asset
     // src: '/public/example.riv',
     canvas: document.getElementById("canvas"),
