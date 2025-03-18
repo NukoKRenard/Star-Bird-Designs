3775ba9bc5152ca97f03f337b6882befaea8dd06
NukoKRenard <skeanobo@gmail.com>
2025-03-18 10:14:27 -0400

    fixed animation dissapearing, preparing to use triggers to play animation on scroll.

--- assets/scripts/aboutme-rive.js
+++ assets/scripts/aboutme-rive.js
@@ -1,11 +1,24 @@
-const processworkanim = new rive.Rive({
+const knightanim = new rive.Rive({
     src: riveanimpath,
     canvas: document.getElementById("birdknight-anim"),
     autoplay: true,
-    stateMachines: "Pull sword",
+    stateMachines: "knight",
     artboard:"aboutme-art",
     onLoad: () => {
-      processworkanim.resizeDrawingSurfaceToCanvas();
+      knightanim.resizeDrawingSurfaceToCanvas();
     },
 });
-window.setInterval(() => {processworkanim.resizeDrawingSurfaceToCanvas()},100);
+window.setInterval(() => {knightanim.resizeDrawingSurfaceToCanvas()},100);
+
+
+const knightobserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+
+    }
+    else {
+
+
+    }
+  })
+})
