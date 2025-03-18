7e3479324f0d9015bc45ee65f0678877a32edfcc
NukoKRenard <skeanobo@gmail.com>
2025-03-18 12:36:05 -0400

    Fixed aboutme animation and added play on scroll effect.

--- assets/scripts/aboutme-rive.js
+++ assets/scripts/aboutme-rive.js
@@ -4,21 +4,27 @@ const knightanim = new rive.Rive({
     autoplay: true,
     stateMachines: "knight",
     artboard:"aboutme-art",
-    onLoad: () => {
+    onLoad: (_) => {
       knightanim.resizeDrawingSurfaceToCanvas();
+
+      const knightinputs = knightanim.stateMachineInputs("knight");
+      
+      const startknightanim = knightinputs.find((input) => input.name === "animate")
+      const resetknightanim = knightinputs.find((input) => input.name === "reset")
+
+      const knightobserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            startknightanim.fire();
+          }
+          else {
+            resetknightanim.fire();
+          }
+        }
+      )
+      },{root: document,rootMargin: "0px",threshold: .5})
+
+      knightobserver.observe(document.getElementById("birdknight-anim"));
     },
 });
 window.setInterval(() => {knightanim.resizeDrawingSurfaceToCanvas()},100);
-
-
-const knightobserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-
-    }
-    else {
-
-
-    }
-  })
-})
